6e8f99ba816e4c27b4a59745909c9264a905089e
satoshisyohu <satoshisyohu@gmail.com>
2023-12-03 12:40:47 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+const app = createApp(App);
+
+//修正
+app.use(router);
+//追加
+app.use(vuetify).mount("#app");
---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <v-btn prepend-icon="$vuetify" variant="outlined">
+      Button
+    </v-btn>  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
